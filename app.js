d227279cfb06ebb648de11c02331e0a0fa207bb0
Parinya <parinya@Parinyas-MacBook-Pro.local>
2018-06-26 17:12:17 +0700

    add body-parser

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 var express = require("express");
 var app = express();
 var mongoose = require("mongoose");
+var bodyParser = require("body-parser");
 
 mongoose.connect("mongodb://localhost/homework_list");
 
 app.set("view engine","ejs");
+app.use(bodyParser.urlencoded({extended:false}));
 app.use(express.static(__dirname+"/public"));
 
 //HomeworkSchema & Homework Model
