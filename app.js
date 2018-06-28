2c53be5c2df4dd8017095761910dbbafb86df214
Parinya <parinya@Parinyas-MacBook-Pro.local>
2018-06-28 23:11:25 +0700

    add Edit Feature, Edit Route and Update Route, install method-override

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ var express = require("express");
 var app = express();
 var mongoose = require("mongoose");
 var bodyParser = require("body-parser");
+var methodOverride = require("method-override");
 
 mongoose.connect("mongodb://localhost/homework_list");
 
 app.set("view engine","ejs");
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static(__dirname+"/public"));
+app.use(methodOverride("_method"));
 
 //HomeworkSchema & Homework Model
 var HomeworkSchema = mongoose.Schema({
@@ -52,6 +54,28 @@ app.post("/homeworks",function(req,res){
   var homework = req.body.homework;
   homework.status = "working";
   Homework.create(homework,function(err,newHomework){
+    if(err){
+      console.log(err);
+    }else{
+      res.redirect("/homeworks");
+    }
+  });
+});
+
+//Edit
+app.get("/homeworks/:id/edit",function(req,res){
+  Homework.findById(req.params.id,function(err,foundHomework){
+    if(err){
+      console.log(err);
+    }else{
+      res.render("homeworks/edit",{homework:foundHomework});
+    }
+  });
+});
+
+//Update
+app.put("/homeworks/:id",function(req,res){
+  Homework.findByIdAndUpdate(req.params.id,req.body.homework,function(err,updateHomework){
     if(err){
       console.log(err);
     }else{
