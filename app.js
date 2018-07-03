812f282302082f3cef95657f76df26761f935f0f
Parinya <parinya@Parinyas-MacBook-Pro.local>
2018-07-03 21:40:45 +0700

    refactor homeworkRoutes

--- app.js
+++ app.js
@@ -6,8 +6,12 @@ var methodOverride = require("method-override");
 var moment = require("moment");
 var Homework = require("./models/homework");
 
-// mongoose.connect("mongodb://localhost/homework_list");
-mongoose.connect(process.env.MONGODBURL);
+
+//Routes
+var homeworkRoutes = require("./routes/homeworks");
+
+mongoose.connect("mongodb://localhost/homework_list");
+// mongoose.connect(process.env.MONGODBURL);
 
 
 app.set("view engine","ejs");
@@ -22,69 +26,7 @@ app.get("/",function(req,res){
   res.redirect("/homeworks");
 });
 
-//-------------------------------- Homeworks -----------------------------------
-//Index
-app.get("/homeworks",function(req,res){
-  Homework.find({},function(err,allHomework){
-    if(err){
-      console.log(err);
-    }else{
-      res.render("homeworks/index",{homeworks:allHomework,moment:moment});
-    }
-  });
-});
-
-//New
-app.get("/homeworks/new",function(req,res){
-  res.render("homeworks/new");
-});
-
-//Create
-app.post("/homeworks",function(req,res){
-  var homework = req.body.homework;
-  homework.status = "working";
-  Homework.create(homework,function(err,newHomework){
-    if(err){
-      console.log(err);
-    }else{
-      res.redirect("/homeworks");
-    }
-  });
-});
-
-//Edit
-app.get("/homeworks/:id/edit",function(req,res){
-  Homework.findById(req.params.id,function(err,foundHomework){
-    if(err){
-      console.log(err);
-    }else{
-      res.render("homeworks/edit",{homework:foundHomework});
-    }
-  });
-});
-
-//Update
-app.put("/homeworks/:id",function(req,res){
-  Homework.findByIdAndUpdate(req.params.id,req.body.homework,function(err,updateHomework){
-    if(err){
-      console.log(err);
-    }else{
-      res.redirect("/homeworks");
-    }
-  });
-});
-
-//Destroy
-app.delete("/homeworks/:id",function(req,res){
-  Homework.findByIdAndRemove(req.params.id,function(err){
-    if(err){
-      console.log(err);
-    }else{
-      res.redirect("/homeworks");
-    }
-  });
-});
-//------------------------------------------------------------------------------
+app.use(homeworkRoutes);
 
 //-------------------------------- Stats ---------------------------------------
 //Index
@@ -102,4 +44,4 @@ app.get("/stats",function(req,res){
 //------------------------------------------------------------------------------
 
 //listen
-app.listen(process.env.PORT,process.env.IP);
+app.listen(3000,process.env.IP);
---
+++ routes/homeworks.js
@@ -0,0 +1,68 @@
+var express = require("express");
+var router = express.Router();
+var Homework = require("../models/homework");
+var moment = require("moment");
+
+//Index
+router.get("/homeworks",function(req,res){
+  Homework.find({},function(err,allHomework){
+    if(err){
+      console.log(err);
+    }else{
+      res.render("homeworks/index",{homeworks:allHomework,moment:moment});
+    }
+  });
+});
+
+//New
+router.get("/homeworks/new",function(req,res){
+  res.render("homeworks/new");
+});
+
+//Create
+router.post("/homeworks",function(req,res){
+  var homework = req.body.homework;
+  homework.status = "working";
+  Homework.create(homework,function(err,newHomework){
+    if(err){
+      console.log(err);
+    }else{
+      res.redirect("/homeworks");
+    }
+  });
+});
+
+//Edit
+router.get("/homeworks/:id/edit",function(req,res){
+  Homework.findById(req.params.id,function(err,foundHomework){
+    if(err){
+      console.log(err);
+    }else{
+      res.render("homeworks/edit",{homework:foundHomework});
+    }
+  });
+});
+
+//Update
+router.put("/homeworks/:id",function(req,res){
+  Homework.findByIdAndUpdate(req.params.id,req.body.homework,function(err,updateHomework){
+    if(err){
+      console.log(err);
+    }else{
+      res.redirect("/homeworks");
+    }
+  });
+});
+
+//Destroy
+router.delete("/homeworks/:id",function(req,res){
+  Homework.findByIdAndRemove(req.params.id,function(err){
+    if(err){
+      console.log(err);
+    }else{
+      res.redirect("/homeworks");
+    }
+  });
+});
+
+module.exports = router;
